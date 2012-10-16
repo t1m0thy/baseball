40080382806829a9b268a35461fe0bfe56b99a37
tim@growdown.com <unknown@kiln.example.com>
2012-10-16 20:31:59 +0400

    installation depency script.  Totally untested and likely incomplete.

---
+++ install/install_dependencies.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y lxml python-lxml libxml2-dev libxslt-dev
+sudo apt-get install -y python-numpy 
+pip install mechanize
+pip install BeautifulSoup
+pip install mechanize
+pip install lxml
+pip install cssselect
+
+
+#pip install nltk
+#sudo apt-get install python-tk
