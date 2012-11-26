df5c7c0a4ea42c29fb7c1a9ebf7ad37ba886400e
tim@growdown.com <unknown@kiln.example.com>
2012-11-26 07:44:53 +0500

    move to beautiful soup 4

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y lxml python-lxml libxml2-dev libxslt-dev
 sudo apt-get install -y python-numpy 
 pip install mechanize
-pip install BeautifulSoup
+pip install BeautifulSoup4
 pip install mechanize
 pip install lxml
 pip install cssselect
@@ -9,3 +9,20 @@ pip install cssselect
 
 #pip install nltk
 #sudo apt-get install python-tk
+
+pip install sqlalchemy
+easy_install -U distribute
+pip install sqlautocode
+
+sudo apt-get install python-mysqldb
+sudo apt-get install libmysqlclient-dev
+
+sudo apt-get install sqlitebrowser
+
+# install local mysql database
+sudo apt-get install mysql-client-5.5
+sudo apt-get install mysql-server-5.5
+
+# creating database of py-gameday
+mysqladmin create gameday -u root -p
+mysql -D gameday < gameday.sql -u root -p
