b6e369cbeb8f7cbf01a7d246c1a630b6b751baec
hirzel <unknown@kiln.example.com>
2013-01-28 05:16:37 +0500

    allow for gzipped web content.

--- src/scrapetools.py
+++ src/scrapetools.py
@@ -1,13 +1,25 @@
 import mechanize
 import os
 import logging
+import gzip
 
 logger = logging.getLogger("scrapetools")
 
 
+def ungzipResponse(r, br):
+    headers = r.info()
+    if headers['Content-Encoding'] == 'gzip':
+        gz = gzip.GzipFile(fileobj=r, mode='rb')
+        html = gz.read()
+        gz.close()
+        headers["Content-type"] = "text/html; charset=utf-8"
+        r.set_data(html)
+        br.set_response(r)
+
+
 def get_cached_url(url, cache_filename=None, force_reload=False):
     if force_reload or cache_filename is None or not os.path.isfile(cache_filename):
-        logging.info("getting game at " + url)
+        logging.info("getting page at " + url)
         br = mechanize.Browser()
         # Browser options
         br.set_handle_equiv(True)
@@ -15,8 +27,12 @@ def get_cached_url(url, cache_filename=None, force_reload=False):
         br.set_handle_referer(True)
         br.set_handle_robots(False)
         br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT6.0; en-US; rv:1.9.0.6')]
+        br.addheaders.append(('Accept-Encoding', 'gzip'))
+
         br.open(url)
-        html = br.response().read()
+        response = br.response()
+        ungzipResponse(response, br)
+        html = response.read()
 
         if cache_filename is not None:
             f = open(cache_filename, 'aw')
